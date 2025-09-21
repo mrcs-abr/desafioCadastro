de0051ab00f63ac7d643ce1f8eb75755d4897e08
Marcos <marcos.br258@gmail.com>
2025-09-21 15:29:54 -0300

    feat: Add creation/reading of formulario.txt

---
+++ src/com/devmrcs/desafiocadastro/dominio/Formulario.java
@@ -0,0 +1,41 @@
+package com.devmrcs.desafiocadastro.dominio;
+
+import java.io.*;
+
+public class Formulario {
+    private String[] perguntas = {
+            "1 - Qual o nome e sobrenome do pet?",
+            "2 - Qual o tipo do pet (Cachorro/Gato)?",
+            "3 - Qual o sexo do animal?",
+            "4 - Qual endereço e bairro que ele foi encontrado?",
+            "5 - Qual a idade aproximada do pet?",
+            "6 - Qual o peso aproximado do pet?",
+            "7 - Qual a raça do pet?"
+    };
+
+    public void read() throws FileNotFoundException, IOException {
+        try (BufferedReader br = new BufferedReader(new FileReader(new File("formulario.txt")))) {
+            String linha;
+            while ((linha = br.readLine()) != null) {
+                System.out.println(linha);
+            }
+        } catch (FileNotFoundException e) {
+            throw new RuntimeException(e);
+        } catch (IOException e) {
+            throw new RuntimeException(e);
+        }
+    }
+
+    public void create() throws IOException {
+        try (BufferedWriter bw = new BufferedWriter(new FileWriter(new File("formulario.txt")))) {
+            for (String pergunta: this.perguntas) {
+                bw.write(pergunta);
+                bw.newLine();
+            }
+            bw.flush();
+        } catch (IOException e) {
+            throw new RuntimeException(e);
+        }
+
+    }
+}
